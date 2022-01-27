9390af38ca54dccf77f07985f5262b754048a050
Alexandra <alexandra.shokhan@gmail.com>
2022-01-27 12:57:37 +0100

    Lesson 1. Test 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,13 @@
 
 public class MainClass {
 
+    private int classNumber = 45;
+
     public int getLocalNumber() {
         return 14;
+    }
+
+    public int getClassNumber() {
+        return this.classNumber;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -7,4 +7,11 @@ public class MainClassTest extends MainClass{
     public void testGetLocalNumber() {
         Assert.assertEquals("Local number does not equal 14", 14, getLocalNumber());
     }
+
+    @Test
+    public void testGetClassNumber() {
+        int max = 45;
+        int testNumber = super.getClassNumber();
+        Assert.assertTrue(String.format("Class number %s is less than 45", testNumber), testNumber >= max);
+    }
 }
